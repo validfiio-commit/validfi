56359ba1245ce2ca01ef4d6469b519a0750170b1
Abhay Agrawal <abhayagrawal0000@gmail..com>
2026-02-25 20:37:47 +0530

    fixed webpack errors

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+config.resolve.alias = {
+  ...config.resolve.alias,
+  "@react-native-async-storage/async-storage": false,
+};
+config.resolve.fallback = {
+  ...config.resolve.fallback,
+  "pino-pretty": false,
+};
 module.exports = nextConfig;
